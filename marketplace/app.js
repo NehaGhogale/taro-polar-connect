2dfe37b538466940def582c624f3f0ea25227ae7
NehaGhogale <ghogale.neha@gmail.com>
2023-02-14 20:00:50 +0530

    express api

---
+++ marketplace/app.js
@@ -0,0 +1,180 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var cors = require('cors')
+
+
+
+const taro = require("./taro_utils");
+
+const app = express();
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json())
+app.use(express.static("public"));
+
+// mint assets
+app.post("/mint-assets", (req, res) => {
+  console.log(req.body);
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    name: req.body.asset_name || "",
+    metadata: req.body.asset_metadata || "",
+    amount: req.body.asset_amount || 1,
+  };
+  taro
+    .mintAsset(
+      reqObj.host,
+      reqObj.macaroon,
+      reqObj.name,
+      reqObj.metadata,
+      reqObj.amount
+    )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+// Connecting gfg-employees database to our express application
+
+// Fetching all the employees
+app.get("/list-assets", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+  };
+  taro
+    .getAssetList(reqObj.host, reqObj.macaroon)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/balance-assets", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+  };
+  taro
+    .getAssetBalance(reqObj.host, reqObj.macaroon)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/new-address", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    genesis_bootstrap_info: req.body.genesis_bootstrap_info || "",
+    amount: req.body.asset_amount || 1,
+  };
+  taro
+    .newNodeAddr(
+      reqObj.host,
+      reqObj.macaroon,
+      reqObj.genesis_bootstrap_info,
+      reqObj.amount
+    )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/send-assets", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    asset_address: req.body.asset_address || "",
+  };
+  taro
+    .sendAssets(reqObj.host, reqObj.macaroon, reqObj.asset_address)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/export-asset-proof", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    asset_id: req.body.asset_id || "",
+    script_key: req.body.script_key || "",
+  };
+  taro
+    .exportProof(
+      reqObj.host,
+      reqObj.macaroon,
+      reqObj.asset_id,
+      reqObj.script_key
+    )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/import-asset-proof", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    raw_proof: req.body.raw_proof || "",
+  };
+  taro
+    .importProof(reqObj.host, reqObj.macaroon, reqObj.raw_proof)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/verify-asset-proof", (req, res) => {
+  let reqObj = {
+    host: req.body.host || "localhost:8089",
+    macaroon: req.body.macaroon || "",
+    raw_proof: req.body.raw_proof || "",
+    genesis_point: req.body.genesis_point || "",
+  };
+  taro
+    .verifyProof(
+      reqObj.host,
+      reqObj.macaroon,
+      reqObj.raw_proof,
+      reqObj.genesis_point
+    )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+// Posting a new employee
+
+app.listen(3005, function () {
+  console.log("Server started on port 3000");
+});
---
+++ marketplace/taro_utils.js
@@ -0,0 +1,202 @@
+const request = require("request");
+
+const get_http_options = (http_host, macaroon) =>{
+  return {
+    url: `https://${http_host}/v1/taro/`,
+    // Work-around for self-signed certificates.
+    rejectUnauthorized: false,
+    json: true,
+    headers: {
+      "Grpc-Metadata-macaroon": macaroon.toString("hex"),
+    },
+  };
+}
+
+const mintAsset = (
+  host,
+  macaroon,
+  name,
+  metadata,
+  amount,
+  asset_type = "COLLECTIBLE",
+  enable_emission = false,
+  skip_batch = true
+) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "assets";
+  let requestBody = {
+    asset_type: asset_type,
+    name: name,
+    meta_data: Buffer.from(JSON.stringify(metadata)).toString("base64"),
+    amount: amount,
+    enable_emission: enable_emission,
+    skip_batch: skip_batch,
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error mininting asset", error);
+        reject(error);
+      } else {
+        console.log("Response minting asset: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const getAssetList = (host, macaroon) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "assets";
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.get(options, function (error, response, body) {
+      if (error) {
+        console.log("Error getting asset", error);
+        reject(error);
+      } else {
+        console.log("Response gtting asset: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const getAssetBalance = (host, macaroon) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "assets/balance";
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.get(options, function (error, response, body) {
+      if (error) {
+        console.log("Error getting asset balances", error);
+        reject(error);
+      } else {
+        console.log("Response gtting asset balances: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const newNodeAddr = (host, macaroon, genesis_bootstrap_info, amount) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "addrs";
+  let requestBody = {
+    genesis_bootstrap_info: genesis_bootstrap_info,
+    amt: amount, // <int64>
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error generating address", error);
+        reject(error);
+      } else {
+        console.log("Response generating address: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const sendAssets = (host, macaroon, asset_address) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "send";
+  let requestBody = {
+    taro_addr: asset_address, // <string>
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error sending asset", error);
+        reject(error);
+      } else {
+        console.log("Response sending asset: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const exportProof = (host, macaroon, asset_id, script_key) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "proofs/export";
+  let requestBody = {
+    asset_id: asset_id,
+    script_key: script_key, // <bytes> (base64 encoded)
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error exporting proof", error);
+        reject(error);
+      } else {
+        console.log("Response exporting proof: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const importProof = (host, macaroon, raw_proof) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "proofs/import";
+  // let proof_file_path = config['PROOF_FILE'] + '/' + asset_id + "/" + script_key + ".taro";
+  let requestBody = {
+    proof_file: raw_proof, //: <string>, // <string>
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error importing proof", error);
+        reject(error);
+      } else {
+        console.log("Response importing proof: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+const verifyProof = (host, macaroon, raw_proof, genesis_point) =>{
+  let options = get_http_options(host, macaroon);
+  options.url += "proofs/verify";
+  let requestBody = {
+    ...raw_proof,
+    ...genesis_point, //: <string>, // <string>
+  };
+  options.form = JSON.stringify(requestBody);
+  console.log("Options: " + JSON.stringify(options));
+  return new Promise((resolve, reject) => {
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("Error verifying proof", error);
+        reject(error);
+      } else {
+        console.log("Response verifying proof: ", JSON.stringify(body));
+        resolve(body);
+      }
+    });
+  });
+}
+
+module.exports = {
+    mintAsset: mintAsset,
+    getAssetList: getAssetList,
+    getAssetBalance: getAssetBalance,
+    newNodeAddr: newNodeAddr,
+    sendAssets: sendAssets,
+    exportProof: exportProof,
+    importProof: importProof,
+    verifyProof: verifyProof
+}
